1e0626ab2c92bd1aebd5c4b19660fbf4429d8a27
akshat <lohia.akshat1609@gmail.com>
2024-03-12 11:41:46 +0530

    app befor firebase

---
+++ lib/first_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:health_connect/screens/signin.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SignIn()));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage("images/f.jpeg"))),
+      ),
+    );
+  }
+}
